832ebec57e5fcb03aff1f6da8ef028805cff192a
Angela Chng <angela@audioreload.com>
2010-11-07 00:00:15 +0800

    screener delete bug

--- app/controllers/screener_playlist_items_controller.rb
+++ app/controllers/screener_playlist_items_controller.rb
@@ -1,2 +1,19 @@
 class ScreenerPlaylistItemsController < ApplicationController
+  before_filter :require_user
+ 	filter_access_to :all
+
+   def new
+     @screenerplaylistitem = ScreenerPlaylistItems.new
+   end
+
+    def destroy
+     @screenerplaylistitem = ScreenerPlaylistItem.find(params[:id])
+     @screenerplaylistitem.destroy
+
+     respond_to do |format|
+   	  flash[:notice] = 'Screener was successfully deleted from playlist.'
+       format.html 
+       format.js
+     end
+   end
 end
