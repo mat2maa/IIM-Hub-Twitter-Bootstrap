001d0867d4a9889b2c63fa910762c9b247861217
Angela Chng <angela@audioreload.com>
2010-12-09 10:37:26 +0800

    add episode number

--- app/controllers/video_master_playlists_controller.rb
+++ app/controllers/video_master_playlists_controller.rb
@@ -209,7 +209,7 @@ class VideoMasterPlaylistsController < ApplicationController
     
     # Master Playlist Summary
     # header row
-    sheet.add_row ["Position", "Programme Title", "Episode Title", "Distributor", "Tape Media", "Tape Format", "Tape Size", "Aspect Ratio", "Language Track 1", "Language Track 2", "Language Track 3", "Language Track 4", "Video Subtitles 1", "Video Subtitles 2", "Master Tape Location", "Master Time In", "Master Time Out", "Duration", "Programme Synopsis", "Episode Synopsis"]
+    sheet.add_row ["Position", "Programme Title", "Episode Title", "Episode Number", "Distributor", "Tape Media", "Tape Format", "Tape Size", "Aspect Ratio", "Language Track 1", "Language Track 2", "Language Track 3", "Language Track 4", "Video Subtitles 1", "Video Subtitles 2", "Master Tape Location", "Master Time In", "Master Time Out", "Duration", "Programme Synopsis", "Episode Synopsis"]
 
     # data rows
     video_master_playlist_items.each do |video_master_playlist_item|
@@ -224,6 +224,7 @@ class VideoMasterPlaylistsController < ApplicationController
         sheet.add_row [video_master_playlist_item.position, 
           video_master_playlist_item.master.video.programme_title, 
           video_master_playlist_item.master.episode_title, 
+          video_master_playlist_item.master.episode_number, 
           distributor, 
           video_master_playlist_item.master.tape_media, 
           video_master_playlist_item.master.tape_format, 
