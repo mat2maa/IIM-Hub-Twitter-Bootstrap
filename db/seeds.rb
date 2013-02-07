77327769efaea64e55574da7a9cf36e7b6282102
Matthew Ager <matthew.a.ager@googlemail.com>
2013-02-07 21:42:02 +0100

    Renamed views to .html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-SupplierCategory.create!(:name => 'Video Distributors', :remarks => '-')
-SupplierCategory.create!(:name => 'Movie Distributors', :remarks => '-')
-SupplierCategory.create!(:name => 'Laboratories', :remarks => '-')
-SupplierCategory.create!(:name => 'Production Studios', :remarks => '-')
+SupplierCategory.create!(name: 'Video Distributors', remarks: '-')
+SupplierCategory.create!(name: 'Movie Distributors', remarks: '-')
+SupplierCategory.create!(name: 'Laboratories',       remarks: '-')
+SupplierCategory.create!(name: 'Production Studios', remarks: '-')
