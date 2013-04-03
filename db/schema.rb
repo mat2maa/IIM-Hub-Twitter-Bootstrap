9bdb3bec565eb9b6ae00eb7aa5bdcef324674b75
Matthew Ager <matthew.a.ager@googlemail.com>
2013-04-04 00:15:56 +0200

    Schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110929060612) do
+ActiveRecord::Schema.define(:version => 20130403171652) do
 
   create_table "airline_rights_countries", :force => true do |t|
     t.string   "name"
