52723ca8bdbd67257cda65b36868d3b0671f3e32
Matthew Ager <matthew.a.ager@googlemail.com>
2013-05-03 15:31:42 +0200

    changed wkhtmltopdf config in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,3 +67,7 @@ Iim::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
+
+PDFKit.configure do |config|
+  config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s
+end
--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,7 +1,7 @@
 require 'pdfkit'
 
 PDFKit.configure do |config|
-  config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf'
+# config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf'
   config.default_options = {
       :page_size => 'A4',
       :print_media_type => true
