58df15fb88c03c01d1e7ebe72ba1b126b57c6f4b
Matthew Ager <matthew.a.ager@googlemail.com>
2013-05-10 14:14:06 +0200

    Moved paperclip config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,18 +66,18 @@ Iim::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # config/environments/production.rb
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
 
 PDFKit.configure do |config|
   config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s
 end
-
-# config/environments/production.rb
-config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
