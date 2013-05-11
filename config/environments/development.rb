ccbb606d7c785dd23903c6f7f865c1724946a13f
Matthew Ager <matthew.a.ager@googlemail.com>
2013-05-11 10:57:31 +0200

    bug fixed when spinner doesn't disappear after delete
    removed local assets precompile as firing JS twice

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Iim::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
   #
 
   # Enable threaded mode
