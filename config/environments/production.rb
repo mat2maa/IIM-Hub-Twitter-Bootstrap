4f645aca99717435e36db41fa8f64d7a13b12ea2
Matthew Ager <matthew.a.ager@googlemail.com>
2013-06-08 21:46:07 +0200

    Moved Thales files from S3 to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,6 @@ PDFKit.configure do |config|
 end
 
 THALES_OPTS = {
-    url: "s3_domain_url",
-    path: "/assets/thales_schema_packages/:id.:extension"
+    url: "public/thales_schema_packages/:id.:extension",
+    path: "public/thales_schema_packages/:id.:extension"
 }
